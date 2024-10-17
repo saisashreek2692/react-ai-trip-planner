96febeaf4ed75033562da4c4261c65d09116db60
Sai Sashreek D <saisashreekd@gmail.com>
2024-10-17 13:06:43 +0530

    Font Family Updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-380bc8ab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6qssgkiqll"
+    "revision": "0.95sgof6sfd"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
